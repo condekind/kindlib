41aa4b145781065519c12c7ba458973056f70702
Bruno Conde Kind <brunocondekind@hotmail.com>
2019-12-05 09:04:23 -0300

    added Colstats

--- fluff.py
+++ fluff.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from IPython.display import display_html
 
 def p(*args):
@@ -6,3 +7,13 @@ def p(*args):
     s += df.head().to_html()
   display_html(s.replace('table','table style="display:inline"'), raw=True)
   return args
+
+def Colstats(df: pd.DataFrame):
+  return pd.DataFrame(data=[(
+    c,
+    mdf[c].dtype,
+    mdf[c].nunique(),
+    mdf[c].isna().sum(),
+    len(mdf[mdf[c].duplicated() == True]))
+    for c in mdf.columns], columns=[
+    'col', 'type', 'nunique', 'nan', 'duplicated_values'])
